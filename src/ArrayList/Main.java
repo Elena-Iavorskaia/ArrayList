2ae904ec8319f370571f45e74db1012d56c39e96
Elena-Iavorskaia <iavorska-o@ukr.net>
2018-05-23 16:52:25 +0300

    First commit

---
+++ src/ArrayList/Main.java
@@ -0,0 +1,20 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+
+        Country indija = new Country("Индия", 500);
+        Country canada = new Country("Канада", 1200);
+        Country japan = new Country("Япония", 700);
+
+        HashMap<Country, String> coun = new HashMap<Country, String>();
+
+
+
+    }
+}
