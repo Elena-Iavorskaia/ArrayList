26e6d7d1b68d274ff70f0dc60656151e99d4d60a
Elena-Iavorskaia <iavorska-o@ukr.net>
2018-05-25 16:53:49 +0300

    Ветка

---
+++ src/main/ArrayList/Country.java
@@ -0,0 +1,11 @@
+package ArrayList;
+
+public class Country {
+    String name;
+    int b;
+
+    public Country(String name, int b) {
+        this.name = name;
+        this.b = b;
+    }
+}
---
+++ src/main/ArrayList/Main.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+
+        Country indija = new Country("Индия", 500);
+        Country canada = new Country("Канада", 1200);
+        Country japan = new Country("Япония", 700);
+
+        HashMap<Country, String> coun = new HashMap<Country, String>();
+        HashMap<Country, String> coun2 = new HashMap<Country, String>();
+int b=55;
+
+
+    }
+}
